832d1a9426ab1bd199a8183941263a2d8df39a67
Bekbolot Tazhibaev <tabekg@gmail.com>
2025-03-14 15:29:29 +0600

    fix

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from endpoints import v1
 from utils.http import make_response
 
-bp = Blueprint('', __name__, url_prefix='')
+bp = Blueprint('__init__', __name__, url_prefix='')
 
 bp.register_blueprint(v1.bp)
 
