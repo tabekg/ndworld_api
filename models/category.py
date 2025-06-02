df54290fafb59dcf71616866d283b46f918785dd
Bekbolot Tazhibaev <tabekg@gmail.com>
2025-06-02 18:05:31 +0600

    models clear

--- models/category.py
+++ models/category.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Column, Boolean
+from sqlalchemy import String, Column, Boolean, Integer, ForeignKey
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import relationship
 from sqlalchemy_json import mutable_json_type
@@ -11,15 +11,18 @@ from utils.http import orm_to_dict
 class Category(Base):
     __tablename__ = 'categories'
 
+    parent_id = Column(Integer, ForeignKey('categories.id', ondelete="CASCADE"), nullable=True)
+
     title = Column(String(255), nullable=False)
     payload = Column(mutable_json_type(dbtype=JSONB, nested=True), nullable=True)
     is_disabled = Column(Boolean, default=False, nullable=False)
 
+    parent = relationship("Category", back_populates="parent", passive_deletes=True)
     resumes = relationship("Resume", secondary=resume_categories, back_populates="categories")
 
     def to_dict_item(self):
         return orm_to_dict(self, [
-            'title', 'type',
+            'title',
             'payload', 'is_disabled',
             'created_at',
         ])
